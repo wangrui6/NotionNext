9bac1240de63daf6efb1e04355094c54b70b2be5
Rui Wang <rwna@amazon.com>
2023-09-23 17:04:32 -0700

    fix typo

--- themes/heo/components/Hero.js
+++ themes/heo/components/Hero.js
@@ -334,19 +334,6 @@ function TodayCard({ cRef }) {
     router.push(CONFIG.HERO_TITLE_LINK)
   }
 
-<<<<<<< HEAD
-  return <div id='today-card' className={`${isCoverUp ? ' ' : 'pointer-events-none'} overflow-hidden absolute hidden xl:flex flex-1 flex-col h-full top-0 w-full`}>
-        <div id='card-body' onClick={handleCardClick} className={`${isCoverUp ? 'opacity-100 cursor-pointer' : 'opacity-0 transform scale-110 pointer-events-none'} shadow transition-all duration-200 today-card h-full bg-[#0E57D5] rounded-xl relative overflow-hidden flex items-end`}>
-            <div id='today-card-info' className='z-10 flex justify-between w-full relative text-white p-10 items-end'>
-                <div className='flex flex-col'>
-                    <div className='text-xs font-light'>{CONFIG.HERO_TITLE_4}</div>
-                    <div className='text-3xl font-bold'>{CONFIG.HERO_TITLE_5}</div>
-                </div>
-                <div onClick={handleClickMore} className={`'${isCoverUp ? '' : 'hidden pointer-events-none '} flex items-center px-3 h-10 justify-center bg-[#425aef] hover:bg-[#4259efcb] transition-colors duration-100 rounded-3xl`}>
-                    <PlusSmall className={'w-6 h-6 mr-2 bg-white rounded-full stroke-indigo-400'} />
-                    <div id='more' className='select-none'>Craving More</div>
-                </div>
-=======
   return (
     <div
       id="today-card"
@@ -382,7 +369,6 @@ function TodayCard({ cRef }) {
             />
             <div id="more" className="select-none">
               更多推荐
->>>>>>> main
             </div>
           </div>
         </div>
